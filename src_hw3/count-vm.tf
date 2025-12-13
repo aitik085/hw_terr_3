0caf0ef8ef720f84f2566f4494259dab6f32e434
Bek Aitkozhoev <baitkozhoev@gmail.com>
2025-12-13 16:52:43 +0000

    3 commit

---
+++ src_hw3/count-vm.tf
@@ -0,0 +1,24 @@
+resource "yandex_compute_instance" "web" {
+count = 2
+name = "web-${count.index+1}"
+platform_id = var.vm_platform_id
+zone        = var.default_zone
+resources {
+    cores         = var.vms_resources["web"].cores
+    memory        = var.vms_resources["web"].memory
+    core_fraction = var.vms_resources["web"].core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_preemptible
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+  metadata = local.vm_metadata
+}
--- src_hw3/variables.tf
+++ src_hw3/variables.tf
@@ -1,8 +1,8 @@
 ###cloud vars
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
+#variable "token" {
+# type        = string
+# description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+#}
 
 variable "cloud_id" {
   type        = string
@@ -29,4 +29,46 @@ variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
+}
+
+variable "vm_image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "Семейство образа для ВМ."
+}
+
+variable "vm_platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "Платформа для ВМ."
+}
+
+variable "vm_preemptible" {
+  type        = bool
+  default     = true
+  description = "Признак прерываемой ВМ."
+}
+
+variable "vm_metadata_base" {
+  description = "Базовые статические метаданные для ВМ."
+  type        = map(string)
+  default = {
+    "serial-port-enable" = "1"
+  }
+}
+
+variable "vms_resources" {
+  description = "Конфигурация ресурсов (CPU, RAM, Core Fraction) для ВМ."
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+}))
+  default = {
+    web = { # Используем 'web' как ключ для ВМ 'web'
+      cores         = 2
+      memory        = 1
+      core_fraction = 20
+    }    
+  }
 }
